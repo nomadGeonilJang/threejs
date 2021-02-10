78cae754337f8d6b821b11f9a4e5902026fa24f2
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-02-11 00:07:30 +0900

    Add control

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
-// Option 1: Import the entire three.js core library.
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,9 +10,15 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
+console.log(controls);
+
+
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( 
+  { color: 0x00ff00, wireframe:true } 
+);
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
