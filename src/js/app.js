b85c988630d281eb99866ce27ebb281d903d73a1
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-02-11 00:29:06 +0900

    Add assets

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
-
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -17,7 +15,7 @@ console.log(controls);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( 
-  { color: 0x00ff00, wireframe:true } 
+  { color: 0xffff00, wireframe:true } 
 );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
