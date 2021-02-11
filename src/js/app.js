e5c8cf3ab384e9148191bc6c411c013f3ebd2c97
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-02-11 17:06:57 +0900

    Add GUI

--- src/js/app.js
+++ src/js/app.js
@@ -1,65 +1,92 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Stats from "three/examples/jsm/libs/stats.module";
+import {GUI} from "three/examples/jsm/libs/dat.gui.module";
+
+let renderer;
+let scene;
+let camera;
 
 
-const renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const init = () =>{
+  renderer = new THREE.WebGLRenderer({antialias:true});
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  document.body.appendChild(renderer.domElement);
 
-const fov = 60; //fielder view
-const aspect = 1920 / 1080;
-const near = 1;
-const far = 1000;
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.set(0,200,335);
+  scene = new THREE.Scene();
+  const ambient = new THREE.AmbientLight(0x00ff00, 0.1);
+  scene.add(ambient);
 
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(75,20,0);
+  const directionalLight = new THREE.DirectionalLight(0xffffff,0.1);
+  scene.add(directionalLight);
 
-const scene = new THREE.Scene();
+  const geoFloor = new THREE.BoxBufferGeometry(2000, 0.1, 2000);
+  const matStdFloor = new THREE.MeshStandardMaterial({
+    color:0x808080,
+    roughness:0,
+    metalness:0
+  });
+
+  const mshStdFloor = new THREE.Mesh(geoFloor,matStdFloor);
+  scene.add(mshStdFloor);
+
+  const matStdObjects = new THREE.MeshStandardMaterial({
+    color:0xffffff,
+    roughness:0,
+    metalness:0,
+  });
+
+  const geoBox = new THREE.BoxBufferGeometry(Math.PI, Math.sqrt(2), Math.E);
+  const mshStdBox = new THREE.Mesh(geoBox, matStdObjects);
+  mshStdBox.position.set(0, 5, 0);
+  mshStdBox.rotation.set(0, Math.PI / 2.0, 0);
+  mshStdBox.castShadow = true;
+  mshStdBox.receiveShadow = true;
+  scene.add(mshStdBox);
+
+  const geoSphere = new THREE.SphereBufferGeometry(1.5, 32, 32);
+  const mshStdSphere = new THREE.Mesh(geoSphere, matStdObjects);
+  mshStdSphere.position.set(-5, 5, 0);
+  mshStdSphere.castShadow = true;
+  mshStdSphere.receiveShadow = true;
+  scene.add(mshStdSphere);
+
+  const geoKnot = new THREE.TorusKnotBufferGeometry(1.5, 0.5, 100, 16);
+  const mshStdKnot = new THREE.Mesh(geoKnot, matStdObjects);
+  mshStdKnot.position.set(5, 5, 0);
+  mshStdKnot.castShadow = true;
+  mshStdKnot.receiveShadow = true;
+  scene.add(mshStdKnot);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.copy(mshStdBox.position);
+  controls.update();
 
 
-const AmbientLightColor = 0xfffffffff;
-const AmbientLightIntensity = 1;
-const light = new THREE.HemisphereLight(0xffff80,0x4040ff,1);
-// const light = new THREE.AmbientLight(AmbientLightColor,AmbientLightIntensity);
-// const light = new THREE.DirectionalLight(0xffffff, 1.0);
-// light.position.set(20,100,10);
-// light.target.position.set(0,0,0);
-// light.castShadow = true;
-scene.add(light);
+  const gui = new GUI({width:300});
+  gui.open();
 
-const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.PlaneGeometry(100,100,10,10);
-const material = new THREE.MeshStandardMaterial({color:0xffffff});
 
-const plane = new THREE.Mesh(geometry, material);
-plane.castShadow = true;
-plane.receiveShadow = true;
-plane.rotation.x = -Math.PI / 2;
-scene.add(plane);
 
+
+
+};
+
+init();
 const stats = new Stats();
 document.body.appendChild(stats.dom);
-
-
-
-const box = new THREE.Mesh(
-  new THREE.SphereGeometry(32,32,32),
-  new THREE.MeshStandardMaterial({
-    color:0xffffff
-  })
-);
-
-box.position.set(0,5,0);
-box.castShadow = true;
-box.receiveShadow = true;
-scene.add(box);
-
-
 const animate = function(){
   requestAnimationFrame(animate);
   stats.update();
